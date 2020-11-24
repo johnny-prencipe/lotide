d55c01d3e3351debf4bf6c3cdaa50deb42774b00
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-24 00:40:39 +0000

    updated head.js after running eslint

--- head.js
+++ head.js
@@ -9,9 +9,8 @@ const assertEqual = (actual, expected) => {
 const head = (arr) => arr[0];
 
 console.log(head([1, 2, 3]));
-console.log(head([typeof(head), false, true]))
-console.log(head([`${assertEqual}`, 1, 'asd']))
-
+console.log(head([typeof(head), false, true]));
+console.log(head([`${assertEqual}`, 1, 'asd']));
 console.log(assertEqual(head([5,6,7]), 5));
 console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
 console.log(head([]));
