5c330ae44d983f63a9f775a862b4f7f93e1759e3
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-24 01:06:10 +0000

    initial upload of tail.js using default test code

---
+++ tail.js
@@ -0,0 +1,27 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return `✅✅✅ Assertion passed. ${actual} === ${expected}`;
+  } else if (actual !== expected) {
+    return `🛑🛑🛑 Assertion failed. ${actual} !== ${expected}`;
+  }
+};
+
+const tail = (arr) => {
+  const outputArr = [];
+  for (let i = 0; i < arr.length; i ++) {
+    if (i !== 0) {
+      outputArr.push(arr[i]);
+    }
+  }
+  return outputArr;
+};
+
+console.log(tail([1, 2, 3]));
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log(assertEqual(result.length, 2)); // ensure we get back two elements
+console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
+console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
