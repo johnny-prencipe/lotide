fec6020d6c5cc98c69ad5001056ee60fabbb39f0
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-25 23:21:13 +0000

    finished findKeyByValue. i really need to get better about making more commits

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return `✅✅✅ Assertion passed. ${actual} === ${expected}`;
+  } else if (actual !== expected) {
+    return `🛑🛑🛑 Assertion failed. ${actual} !== ${expected}`;
+  }
+};
+
+const findKeyByValue = (inputObj, value) => {
+  for (let key of Object.keys(inputObj)) {
+    if (inputObj[key] === value) {
+      return key;
+    }
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama")); //pass
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined)); //pass
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama")); // should fail
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi")); //pass
