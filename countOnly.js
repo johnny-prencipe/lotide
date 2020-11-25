5d990509d075d243ca3a4f7207124525eda7ee2a
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-25 21:52:29 +0000

    initial push of countOnly w/ test code written but no functionality

---
+++ countOnly.js
@@ -0,0 +1,30 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return `✅✅✅ Assertion passed. ${actual} === ${expected}`;
+  } else if (actual !== expected) {
+    return `🛑🛑🛑 Assertion failed. ${actual} !== ${expected}`;
+  }
+};
+
+const countOnly = (allItems, itemsToCount) => {
+  const countedItems = {};
+
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+console.log(assertEqual(result1["Karima"], undefined));
+console.log(assertEqual(result1["Jason"], 1));
+console.log(assertEqual(result1["Fang"], 2));
+console.log(assertEqual(result1["Agouhanna"], undefined));
