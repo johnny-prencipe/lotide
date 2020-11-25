e1ac09c471910c1d79cf9e98f586a4896da6b057
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-25 23:07:26 +0000

    finished letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,17 @@
+const letterPositions = (sentence) => {
+  const results = {};
+
+  for (let i in sentence.toLowerCase()) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(Number(i));
+
+    } else if (!results[sentence[i]] && sentence[i] !== ' ') {
+      results[sentence[i]] = [Number(i)];
+      
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions('lighthouse in the house'));
+console.log(letterPositions('hello'));
