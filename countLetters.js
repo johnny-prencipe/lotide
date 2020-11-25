91e3ffb590c9e838bed14f9597356864c1c36191
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-25 22:47:05 +0000

    finished countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -6,3 +6,18 @@ const assertEqual = (actual, expected) => {
   }
 };
 
+const countLetters = (sentence) => {
+  let returnObj = {};
+  for (let i of sentence.toLowerCase()) {
+    if (returnObj[i]) {
+      returnObj[i] += 1;
+    } else if (!returnObj[i] && i != ' ') { // better way to do this with
+      //regex, need to watch some videos on it before i'm 
+      //comfortable using it though
+      returnObj[i] = 1;
+    }
+  }
+  console.log(returnObj);
+}
+
+countLetters('The quick brown fox jumps over the lazy dog');
