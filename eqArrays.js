0dcf67ff41df87492bfbc790c01e2a833d047966
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-25 00:42:24 +0000

    updated eqArrays.js file, should be working

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return `✅✅✅ Assertion passed. ${actual} === ${expected}`;
+  } else if (actual !== expected) {
+    return `🛑🛑🛑 Assertion failed. ${actual} !== ${expected}`;
+  }
+};
+
+const eqArrays = (firstArr, secondArr) => {
+  //off the bat return flase if they're different lengths
+  if (firstArr.length != secondArr.length) {
+    return false;
+  }
+  //add a for-in loop to check each index in both arrays 
+  //against each other, item by item.
+  //if there's ever a mismatch, return false
+  for (let i in firstArr) {
+    if (firstArr[i] !== secondArr[i]) {
+      return false;
+    }
+  }
+  //return true after the previous checks pass.
+  return true;
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
