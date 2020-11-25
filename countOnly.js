bf4f9a48eff7a5a3b792e52cdb821dc34a1a06a2
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-25 22:21:23 +0000

    added logic to count every item

--- countOnly.js
+++ countOnly.js
@@ -10,9 +10,14 @@ const countOnly = (allItems, itemsToCount) => {
   const results = {};
 
   for (const item of allItems) {
-    console.log(item);
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
   }
 
+  console.log(results);
   return results;
 }
 
