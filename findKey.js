27f980b5aca392c71098fb55fe0ca34a7139b2fd
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-26 22:48:10 +0000

    finished findKey.js

--- findKey.js
+++ findKey.js
@@ -1,7 +1,20 @@
 const findKey = (object, callback) => {
   for (let key in object) {
-    console.log(key)
+    if (callback(object[key])) {
+      return key;
+    }
   }
-}
+};
 
-findKey({a: 1, b: 2, c: 3}, map());
+console.log(findKey({a: 1, b: 2, c: 3}, x => x === 2)); //'b'
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
+
+console.log(findKey({a: 'a', b: 'b', c: 'c'}, x => typeof(x) !== 'string')); //should fail
