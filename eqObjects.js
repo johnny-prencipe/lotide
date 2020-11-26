bc7efc700aea1504ae513c3032cfe38c161665ee
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-26 02:01:50 +0000

    refactored

--- eqObjects.js
+++ eqObjects.js
@@ -26,18 +26,21 @@ const eqArrays = (firstArr, secondArr) => {
 const eqObjects = (object1, object2) => {
   // Returns true if both objects have identical keys with identical values.
   // Otherwise you get back a big fat false!
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
+  const objOneKeys = Object.keys(object1);
+  const objTwoKeys = Object.keys(object2);
+  if (objOneKeys.length !== objTwoKeys.length) {
     return false;
-
-  } else {
-    for (let key of Object.keys(object1)) {
-      if (Array.isArray(object1[key]) &&  Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else if (object1[key] !== object2[key]) {
+  }
+  
+  for (let key of objOneKeys) {
+    if (Array.isArray(object1[key]) &&  Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
+    }
+
+    if (object1[key] !== object2[key]) {
+      return false;
     }
   }
   return true;
