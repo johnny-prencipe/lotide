f69048bdbedf4c88613b1795e01aa77381d39bef
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-30 21:32:20 +0000

    refactored eqArrays & made eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    return `✅✅✅ Assertion passed. ${actual} === ${expected}`;
-  } else if (actual !== expected) {
-    return `🛑🛑🛑 Assertion failed. ${actual} !== ${expected}`;
-  }
-};
-
 const eqArrays = (firstArr, secondArr) => {
   //off the bat return flase if they're different lengths
   if (firstArr.length !== secondArr.length) {
@@ -23,10 +15,4 @@ const eqArrays = (firstArr, secondArr) => {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
+module.exports = (eqArrays);
