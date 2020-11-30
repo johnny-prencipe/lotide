dfd8ba0dcb77d483c75a1fd33e50d3b9ac6cff92
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-11-30 22:56:29 +0000

    refactored tailTest.js to use mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,22 @@
+const { italic } = require('ansi-colors');
 const tail = require('../tail.js');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-console.log(tail([1, 2, 3]));
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result.length, 2)); // ensure we get back two elements
-console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
-console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
+describe('#tail', () => {
+  it('returns [2, 3] given [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it('returns ["Lighthouse", "Labs"] when passed ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it ('returns 2 elements when given an array of 3', () => {
+    assert.lengthOf(tail([1, 2, 3]), 2);
+  });
+  it('returns empty array if length is equal to 1', () => {
+    assert.lengthOf(tail([1]), 0);
+  });
+  it('returns empty array if length is equal to 0', () => {
+    assert.lengthOf(tail([]), 0);
+  });
+});
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
